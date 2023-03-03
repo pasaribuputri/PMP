449053eb8efad6ec07d5baa0d1e262299ad9c627
putri mulyani <pasaribuputri02@gmail.com>
2023-03-03 21:08:55 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+import express from "express";
+
+const app = express();
+
+// middleware
+
+app.use((req,res,next) =>{
+    console.log("ada request masuk");
+    console.log(req.url); //untuk mencetak request
+    next();
+})
+
+// route 
+
+app.get("/", (req,res)=>{
+    res.send("Putri Mulyani")
+})
+
+app.listen(3000, ()=>{
+    console.log("server running at port 3000")
+})
